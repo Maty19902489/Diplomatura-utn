59692d6045329c5cf08b37055ef5a33a783e35dc
Matias Robles <roblesmatias31@gmail.com>
2022-02-02 10:04:24 -0300

    añado texto y pequeños cambios de estilo al formulario

--- Proyecto final/app.js
+++ Proyecto final/app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".header__menu");
 const openMenuBoton = document.querySelector(".open__menu");
 const closeMenuBoton = document.querySelector(".close__menu");
+const formularioBoton = document.querySelector("main__exchange-formulario");
 
 function toggleMenu () {
     menu.classList.toggle("menu_opened")
@@ -8,3 +9,19 @@ function toggleMenu () {
 
 openMenuBoton.addEventListener("click", toggleMenu);
 closeMenuBoton.addEventListener("click", toggleMenu);
+ocultarMenu.addEventListener("click", toggleMenu);
+
+
+/* var openMenuBoton = document.getElementById("header__menu");
+    closeMenuBoton = document.getElementById("close__menu");
+     openMenuBoton = document.getElementById("open__menu");
+     formularioBoton = document.getElementById("main__exchange-formulario");
+
+     function ocultar(){
+         header__menu.addEventListener("click", toggleMenus);
+         close__menu.addEventListener("click", toggleMenu);
+         open__menu.addEventListener("click", toggleMenu);
+         main__exchange-formulario.addEventListener("click", toggleMenu);
+     }
+    
+     header__menu.addEventListener("click",ocultar) */
