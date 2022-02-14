404652f092c8c8fbc3717add7cd03e4723645fd1
Matias Robles <roblesmatias31@gmail.com>
2022-02-13 22:26:49 -0300

    Ejercicios unidad 4 modulo 2

---
+++ M3 u2/Ejercicio 1/app.js
@@ -0,0 +1,13 @@
+var color = document.getElementById('caja1');
+
+color.addEventListener('click', cambiarColor)
+{
+    color.style = 'backgroundColor: green;'
+};
+
+
+
+
+function cambiarColor() {
+    color.style = 'backgroundColor: green;'
+}
